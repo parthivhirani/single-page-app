a4b78c40399f623bd6406cb24324dfa2c4f26899
Scientific Calculator | TS <parthivhirani2903@gmail.com>
2023-03-30 13:33:52 +0530

    Used different javascript method to scroll the web site

--- spa.js
+++ spa.js
@@ -1,19 +1,127 @@
 location.href = "#";
 
 function home() {
-    location.href = "#";
+    // METHOD-1
+    // location.href = "#name";
+
+    // METHOD-2
+    // window.scrollTo(0, 0);
+
+    // METHOD-3
+    document.getElementById("name").scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center"
+    });
+
+    // METHOD-4
+    // window.scrollTo(0, findPosition(document.getElementById("name")));
+
+    // METHOD-5
+    // window.scroll(0, findPosition(document.getElementById("name")));
+
+    // function findPosition(obj) {
+    //   var currenttop = 0;
+    //   if (obj.offsetParent) {
+    //     do {
+    //       currenttop += obj.offsetTop;
+    //     } while ((obj = obj.offsetParent));
+    //     return [currenttop];
+    //   }
+    // }
 }
 
 function aboutus() {
-    location.href = "#aboutus";
+    // METHOD-1
+    // location.href = "#aboutus";
+
+    // METHOD-2
+    // window.scrollTo(0, 1700);
+
+    // METHOD-3
+    document.getElementById("aboutus").scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center"
+    });
+
+    // METHOD-4
+    // window.scrollTo(0, findPosition(document.getElementById("aboutus")));
+
+    // METHOD-5
+    // window.scroll(0, findPosition(document.getElementById("aboutus")));
+
+    // function findPosition(obj) {
+    //   var currenttop = 0;
+    //   if (obj.offsetParent) {
+    //     do {
+    //       currenttop += obj.offsetTop;
+    //     } while ((obj = obj.offsetParent));
+    //     return [currenttop];
+    //   }
+    // }
 }
 
 function images1() {
-    location.href = "#images1";
+    // METHOD-1
+    // location.href = "#images1";
+
+    // METHOD-2
+    // window.scrollTo(0, 2050);
+
+    // METHOD-3
+    document.getElementById("images1").scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center"
+    });
+
+    // METHOD-4
+    // window.scrollTo(0, findPosition(document.getElementById("images1")));
+
+    // METHOD-5
+    // window.scroll(0, findPosition(document.getElementById("images1")));
+
+    // function findPosition(obj) {
+    //   var currenttop = 0;
+    //   if (obj.offsetParent) {
+    //     do {
+    //       currenttop += obj.offsetTop;
+    //     } while ((obj = obj.offsetParent));
+    //     return [currenttop];
+    //   }
+    // }
 }
 
 function services() {
-    location.href = "#services";
+    // METHOD-1
+    // location.href = "#services";
+
+    // METHOD-2
+    // window.scrollTo(0, 3000);
+
+    // METHOD-3
+    document.getElementById("services").scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center"
+    });
+
+    // METHOD-4
+    // window.scrollTo(0, findPosition(document.getElementById("services")));
+
+    // METHOD-5
+    // window.scroll(0, findPosition(document.getElementById("services")));
+
+    // function findPosition(obj) {
+    //   var currenttop = 0;
+    //   if (obj.offsetParent) {
+    //     do {
+    //       currenttop += obj.offsetTop;
+    //     } while ((obj = obj.offsetParent));
+    //     return [currenttop];
+    //   }
+    // }
 }
 
 // SLIDING IMAGES
